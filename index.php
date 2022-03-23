77d76d557af0a71428367173bf0f736fb6f08045
AdrtH <90867832+AdrtH@users.noreply.github.com>
2022-03-23 19:43:36 +0100

    création de game.php + ajout form dans index

--- index.php
+++ index.php
@@ -3,10 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HYSTORITSDFQEQ</title>
+    <title>HYSTORIC</title>
 </head>
 <body>
-    <h1>MAGIC</h1>
-    <p>LOL</p>
+    <h1>Hystoric</h1>
+    
+    
+    <form id="Form" action="game.php" method="post">
+    </form>
+    <script>
+        for (let index = 0; index < 5; index++) {
+            document.getElementById("Form").innerHTML += ("<input type='radio' name='Deck' id='"+index+"'>Deck "+(1+index)+"</input><br>");
+        }
+        document.getElementById("Form").innerHTML += ("<input type='submit' value='JOUER !!'>");
+    </script>
 </body>
 </html>
