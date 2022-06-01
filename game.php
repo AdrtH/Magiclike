02221798e61662235fd3ee91787b1ce71089a10b
Nigerundewon <radewoneld@gmail.com>
2022-06-01 16:54:23 +0200

    changement localhost

--- game.php
+++ game.php
@@ -11,9 +11,9 @@
     <?php
         //echo "<p>deck ".$_POST['Deck']."</p>";
 
-        $servername = "drimtim.wstr.fr";
-        $username   = "drimtim";
-        $password   = "!drimtim6969#";
+        $servername = "localhost";
+        $username   = "root";
+        $password   = "";
         $dbname     = "hystoric";
         $test       = "oui";
 
@@ -45,7 +45,7 @@
         }
 
         // crée une boucle qui va parcourir toutes les appartencances de carte dans le deck choisi, 
-        $query = "SELECT carte.card_id as id, carte.name as name, carte.cost as cost, carte.description as descr, carte.type as type, carte.onPlay as onPlay, carte.eachTurn as eachTurn, carte.onTap as onTap, carte.onDie as onDie
+        $query = "SELECT carte.card_id as id, carte.name as name, carte.cost as cost, carte.description as descr, carte.type as type, carte.onPlay as onPlay, carte.eachTurn as eachTurn, carte.onTap as onTap, carte.onDeath as onDie
                   FROM carte
                   INNER JOIN app_carte as app 
                   ON carte.card_id = app.card_id
